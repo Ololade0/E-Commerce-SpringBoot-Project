fd1f898c534c95a554a0749bce0b3459cd562cef
Ololade0 <adesuyiololade@gmail.com>
2022-12-28 19:47:43 +0100

    E-commerce project

--- src/main/java/africa/semicolon/lumexpress/security/config/SecurityConfig.java
+++ src/main/java/africa/semicolon/lumexpress/security/config/SecurityConfig.java
@@ -41,20 +41,6 @@ public class SecurityConfig {
                 .build();
     }
 
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source =
-//                new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
-
-
 
 
 }
